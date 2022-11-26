e7673f25d6162ccbe9bf7a099952b30a9068e888
akursus <ak_ursus@list.ru>
2022-11-26 20:08:11 +0300

    Task 1.3

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,16 @@ public class Main {
         for (int i = 0; i <= 10; i ++) {
             System.out.println(i);
         }
+
         //Task 1.2
         System.out.println("Задание 1.2");
         for (int i = 10; i >= 1; i --) {
+            System.out.println(i);
+        }
+
+        //Task 1.3
+        System.out.println("Задание 1.3");
+        for (int i = 0; i <= 17; i = i+2) {
             System.out.println(i);
         }
 

061568e33a3f9175c4cd8964672a8107ba5e1d0f
akursus <ak_ursus@list.ru>
2022-11-26 20:03:21 +0300

    Task 1.1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task 1.1
+        System.out.println("Задание 1.1");
+        for (int i = 0; i <= 10; i ++) {
+            System.out.println(i);
+        }
+
+
+    }
+}

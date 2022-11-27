186e926b6bf2ed441ecc41a12223b760c88edc6a
akursus <ak_ursus@list.ru>
2022-11-27 09:29:24 +0300

    Task 2.1 corrected

--- src/Main.java
+++ src/Main.java
@@ -26,8 +26,9 @@ public class Main {
 
         //Task 2.1
         System.out.println("Задание 2.1");
-        for (int i = 1904; i <= 2096; i = i+4) {
-            System.out.println(i + " год является високосным");
+        for (int year = 1904; year <= 2096; year ++) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            System.out.println(year + " год является високосным");
         }
 
         //Task 2.2
